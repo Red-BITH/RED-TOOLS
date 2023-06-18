5cc7c0da3e5b6f83cf74070f2571eb7e256f5e52
Red-BITH <136892734+Red-BITH@users.noreply.github.com>
2023-06-18 00:40:11 -0700

    SlowTooLL.py
    LASTEST VERSİON

--- SlowTooLL.py
+++ SlowTooLL.py
@@ -32,7 +32,7 @@ def print_colored(text, color):
 
 
 def durdur(signal, frame):
-    print("Program kapatılıyor...")
+    print("Program söndürülür...")
     os._exit(0)
 
 # Ctrl+C sinyalini yakalamak için signal modülünü kullanıyoruz
