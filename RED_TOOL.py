45fbbf72d4aa88f2e1aa819d6b4e1cca0529df61
Red-BITH <136892734+Red-BITH@users.noreply.github.com>
2023-06-26 04:58:49 -0700

    Update RED_TOOL.py

--- RED_TOOL.py
+++ RED_TOOL.py
@@ -199,7 +199,7 @@ elif secim == '4':
 elif(secim == "5"):
     os.system("clear")
     os.system("cd TOXUNMA")
-    os.system("python system.py")
+    
 
 else:
     print_colored("----Xəta baş verdi!", Colors.RED)
