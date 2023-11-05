0aefde16a2693465066b3e9559912c0172c747ec
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-11-05 05:12:48 -0800

    Update RED_TOOL.py

--- RED_TOOL.py
+++ RED_TOOL.py
@@ -354,26 +354,26 @@ if(dil == "1"):
          own_ip_info = get_own_ip_info()
          print_colored(json.dumps(own_ip_info, indent=4), Colors.YELLOW)
 
-     elif secim3 == '2':
-         rabite = input("İP daxil et")
-         url = "https://ipinfo.io/" + rabite
-         response = urlopen(url)
-         data = json.load(response)
-
-         table_data = [
-             ["IP", data["ip"]],
-             ["city", data["city"]],
-             ["Region", data["region"]],
-             ["Country", data["country"]],
-             ["Postal Code", data["postal"]],
-             ["Organization", data["org"]],
-             ["ASN", data.get("asn", ["N/A"])[0]],
-             ["IP Range", data.get("ip_range", "N/A")],
-             ["Local Time", data.get("timezone", "N/A")],
-             ["Timezone", data.get("timezone", "N/A")],
-             ["Coordinates", data.get("loc", "N/A")],
-             ["Privacy Detection", data.get("privacy", "N/A")]
-         ]
+     if(secim3 == '2'):
+      rabite = input("İP daxil et")
+      url = "https://ipinfo.io/" + rabite
+      response = urlopen(url)
+      data = json.load(response)
+      
+      table_data = [
+       ["IP", data["ip"]],
+       ["city", data["city"]],
+       ["Region", data["region"]],
+       ["Country", data["country"]],
+       ["Postal Code", data["postal"]],
+       ["Organization", data["org"]],
+       ["ASN", data.get("asn", ["N/A"])[0]],
+       ["IP Range", data.get("ip_range", "N/A")],
+       ["Local Time", data.get("timezone", "N/A")],
+       ["Timezone", data.get("timezone", "N/A")],
+       ["Coordinates", data.get("loc", "N/A")],
+       ["Privacy Detection", data.get("privacy", "N/A")]
+       ]
 
          table = tabulate(table_data, headers=["Field", "Value"], tablefmt="grid")
          print_colored(table, Colors.BLUE)
