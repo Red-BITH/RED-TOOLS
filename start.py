2d5ef42561571ead9dbb9135d901e74fc287bebd
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-11-06 03:49:49 -0800

    Create start.py

---
+++ start.py
@@ -0,0 +1,48 @@
+import time
+import os
+os.system("clear")
+print("""\033[31m\
+
+
+ ▄▄▄▄    ██▓     ▒█████   ▒█████  ▓█████▄ ▓█████  ██▀███  
+▓█████▄ ▓██▒    ▒██▒  ██▒▒██▒  ██▒▒██▀ ██▌▓█   ▀ ▓██ ▒ ██▒
+▒██▒ ▄██▒██░    ▒██░  ██▒▒██░  ██▒░██   █▌▒███   ▓██ ░▄█ ▒
+▒██░█▀  ▒██░    ▒██   ██░▒██   ██░░▓█▄   ▌▒▓█  ▄ ▒██▀▀█▄  
+░▓█  ▀█▓░██████▒░ ████▓▒░░ ████▓▒░░▒████▓ ░▒████▒░██▓ ▒██▒
+░▒▓███▀▒░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒▒▓  ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░
+▒░▒   ░ ░ ░ ▒  ░  ░ ▒ ▒░   ░ ▒ ▒░  ░ ▒  ▒  ░ ░  ░  ░▒ ░ ▒░
+ ░    ░   ░ ░   ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░  ░    ░     ░░   ░ 
+ ░          ░  ░    ░ ░      ░ ░     ░       ░  ░   ░     
+      ░                            ░                     
+""")
+time.sleep(0.5)
+print("""\033[31m
+      DİL SEÇ \ CHOOSE LANGUAGE
+
++++++++++++++++++++++++++
++  AZ. DİLİ ÜÇÜN ---1   +
++  US. FOR ENG ---2     +
++  TR. DİLİ İÇİN ---3   +
++++++++++++++++++++++++++
+""")
+dil = input("Yaz\Write: ")
+if(dil == "1"):
+ print("""\033[33m
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ +DİQQƏT!! SİZ BU DƏQİQƏ STANDART PAKETDƏSİNİZ.       + 
+ +EYER PREMİUM TOKENİNİZ VARSA TOKEN DAXİT ETME YERİNE+
+ +DAXİL EDİN.YOXDURSA "yox" yaz.DAVAM ET(dırnaqsız)   +
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ """)
+ token = input("Token Daxil et:::")
+if(token == "3qk33xq8qmicptrev19i" or "pqc7rfd4d056i3lf67ym" or "yox" ):
+  os.system("python cd.py")
+else:
+  print("SEHV TOKEN YADA SİMVOL! DAXİL EDİLDİ!")
+
+
+
+
+
+
+  
