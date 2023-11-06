c89b12aecfc4e6f7a06fb55df64781c9caeb64d5
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-11-06 03:34:42 -0800

    Update RED_TOOL.py

--- RED_TOOL.py
+++ RED_TOOL.py
@@ -28,11 +28,11 @@ print("""\033[31m
 dil = input("Yaz\Write: ")
 if(dil == "1"):
  print("""\033[33m
- +++++++++++++++++++++++++++++++++++++++++++++++++++
- +DİQQƏT!! SİZ BU DƏQİQƏ STANDART PAKETDƏSİNİZ.    + 
- +EYER TOKENİNİZ VARSA TOKEN DAXİT ETME YERİNE     +
- +DAXİL EDİN.YOXDURSA "yox" yaz.DAVAM ET(dırnaqsız)+
- +++++++++++++++++++++++++++++++++++++++++++++++++++
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ +DİQQƏT!! SİZ BU DƏQİQƏ STANDART PAKETDƏSİNİZ.       + 
+ +EYER PREMİUM TOKENİNİZ VARSA TOKEN DAXİT ETME YERİNE+
+ +DAXİL EDİN.YOXDURSA "yox" yaz.DAVAM ET(dırnaqsız)   +
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++
  """)
  token = input("Token Daxil et:::")
 if(token == "3qk33xq8qmicptrev19i" or "pqc7rfd4d056i3lf67ym" or "yox" ):
